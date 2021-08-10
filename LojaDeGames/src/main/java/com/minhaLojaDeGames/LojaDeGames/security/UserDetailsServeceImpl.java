43eb2835ca5864a644d272add602f05628db828c
TamiresGS <tamires_app@hotmail.com>
2021-08-10 17:08:22 -0300

    Inserindo camada Security

---
+++ LojaDeGames/src/main/java/com/minhaLojaDeGames/LojaDeGames/security/UserDetailsServeceImpl.java
@@ -0,0 +1,30 @@
+package com.minhaLojaDeGames.LojaDeGames.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.minhaLojaDeGames.LojaDeGames.Repository.UsuarioRepository;
+import com.minhaLojaDeGames.LojaDeGames.model.Usuario;
+
+
+	@Service 
+	public class UserDetailsServeceImpl implements UserDetailsService {
+
+		@Autowired
+		private UsuarioRepository userRepository;
+		
+		@Override
+		public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+			Optional<Usuario> user = userRepository.findByUsuario(userName);
+			user.orElseThrow(() -> new UsernameNotFoundException (userName + " not found."));
+			
+			return user.map(UserDetailsImpl::new).get();
+			
+		}
+		
+}
